c71dc597f94d1c46d68768fbb405ed469023cb32
1aimaneqbal <1aimaneqbal@gmail.com>
2021-04-27 02:04:38 +0530

    Custom Cursor

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const mnavlinks = document.querySelector(".mnavlinks");
 hamburger.addEventListener("click", () => {
     mnavlinks.classList.toggle("open");
 });
+
+const cursorRef = document.querySelector(".cursor"); 
+const cursorRef2 = document.querySelector(".cursor2"); 
+
+document.addEventListener("mousemove", (e) =>{
+    cursorRef.setAttribute("style", `top: ${e.pageY - 10}px; left: ${e.pageX - 10}px;`);
+    cursorRef2.setAttribute("style", `top: ${e.pageY - 10}px; left: ${e.pageX - 10}px;`);
+})
+document.addEventListener("click", () => {
+    cursorRef2.classList.add("expand");
+    setTimeout(() => {
+        cursorRef2.classList.remove("expand");
+    }, 500);
+});
