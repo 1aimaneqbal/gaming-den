4adfd456c1f31d38d4bd627ce324eeb758d8054f
1aimaneqbal <1aimaneqbal@gmail.com>
2021-05-02 03:36:12 +0530

    Added Excluives using Swiper.js & Updated README

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
 # Gaming Den
 
 Checkout the live version [here](https://1aimaneqbal.github.io/gaming-den/).
+
+## Things that I've used while making this project
+
+### HTML elements
+
+• Forms
+• Linking - External & Internal
+• Div, section & nav
+• Use of classes and ids
+• SVGs
+• Unordered List
+• Image
+• Headings
+• Paragraphs
+• Span
+
+### CSS props
+
+• Positions - Relative, Absolute & Fixed
+• Display - Flex, Blocks, Inline & None
+• Psuedo Elements & Classes - ::before, :valid, :focus & :hover
+• Colors
+• Background & Background-color
+• Box Model
+• Margin & Padding
+• Border and Outline
+• Rounded Corners
+• Gradients
+• Shadows
+• Font Styling - Size & Weight
+• Transition
+• Transform - Translate, Scale & Opactity
+• Animations
+• Filter - Blur & Greyscale
+• Clipping
+
+### JavaScript manipulations
+
+• DOM manipulations using queryselector
+
+### Library Used
+
+• Swiper.js
+
+### Misc
+
+• SVG animation using CSS with `stroke-dasharray` & `stroke-dashoffset` styling property.
--- app.js
+++ app.js
@@ -22,3 +22,41 @@ document.addEventListener("click", () => {
         cursorRef2.classList.remove("expand");
     }, 500);
 });
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+    },
+    loop: true,
+});
+const closeexcps = document.querySelector(".close-exc-ps");
+const psgamesection = document.querySelector(".psgamesection");
+
+const openpsexc = () => {
+    psgamesection.classList.toggle("psgamesectionopen");
+}
+
+closeexcps.addEventListener("click", () =>{
+    psgamesection.classList.toggle("psgamesectionopen");
+})
+const closeexcxbox = document.querySelector(".close-exc-xbox");
+const xboxgamesection = document.querySelector(".xboxgamesection");
+
+const openxboxexc = () => {
+    xboxgamesection.classList.toggle("xboxgamesectionopen");
+}
+
+closeexcxbox.addEventListener("click", () =>{
+    xboxgamesection.classList.toggle("xboxgamesectionopen");
+})
+
+const buyNow = () => {
+    alert("Sorry, this is currently out of stock.");
+}
