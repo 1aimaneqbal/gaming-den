0be10a2cacbbd2481c560f4d699e80e196b94d49
1aimaneqbal <1aimaneqbal@gmail.com>
2021-05-05 21:38:37 +0530

    Mouse now follows on Scroll

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const closeham = document.querySelector(".closeham");
 const mnavlinks = document.querySelector(".mnavlinks");
-
 hamburger.addEventListener("click", () => {
     mnavlinks.classList.toggle("open");
 });
@@ -11,10 +10,17 @@ closeham.addEventListener("click", () => {
 
 const cursorRef = document.querySelector(".cursor"); 
 const cursorRef2 = document.querySelector(".cursor2"); 
-
+let topY;
+let leftX;
 document.addEventListener("mousemove", (e) =>{
+    topY = e.clientY;
+    leftX = e.clientX
     cursorRef.setAttribute("style", `top: ${e.pageY - 10}px; left: ${e.pageX - 10}px;`);
     cursorRef2.setAttribute("style", `top: ${e.pageY - 10}px; left: ${e.pageX - 10}px;`);
+})
+document.addEventListener("scroll", (e) =>{
+    cursorRef.setAttribute("style", `top: ${Math.round(document.documentElement.scrollTop) + topY - 10}px; left: ${leftX - 10}px;`);
+    cursorRef2.setAttribute("style", `top: ${Math.round(document.documentElement.scrollTop) + topY - 10}px; left: ${leftX - 10}px;`);
 })
 document.addEventListener("click", () => {
     cursorRef2.classList.add("expand");
@@ -22,6 +28,7 @@ document.addEventListener("click", () => {
         cursorRef2.classList.remove("expand");
     }, 500);
 });
+
 var swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
     grabCursor: true,
@@ -36,25 +43,35 @@ var swiper = new Swiper(".mySwiper", {
     },
     loop: true,
 });
+
 const closeexcps = document.querySelector(".close-exc-ps");
 const psgamesection = document.querySelector(".psgamesection");
-
 const openpsexc = () => {
     psgamesection.classList.toggle("psgamesectionopen");
 }
-
 closeexcps.addEventListener("click", () =>{
     psgamesection.classList.toggle("psgamesectionopen");
 })
+
 const closeexcxbox = document.querySelector(".close-exc-xbox");
 const xboxgamesection = document.querySelector(".xboxgamesection");
-
 const openxboxexc = () => {
     xboxgamesection.classList.toggle("xboxgamesectionopen");
 }
-
 closeexcxbox.addEventListener("click", () =>{
     xboxgamesection.classList.toggle("xboxgamesectionopen");
+})
+closeexcps.addEventListener("click", () =>{
+    psgamesection.classList.toggle("psgamesectionopen");
+})
+
+const closeexcpc = document.querySelector(".close-exc-pc");
+const gridlayout = document.querySelector(".gridlayout");
+const openpcexc = () => {
+    gridlayout.classList.toggle("gridlayoutopen");
+}
+closeexcpc.addEventListener("click", () =>{
+    gridlayout.classList.toggle("gridlayoutopen");
 })
 
 const buyNow = () => {
